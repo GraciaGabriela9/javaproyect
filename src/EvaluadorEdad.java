a463277a336c8bb76801ad6f531c5991d243ac2a
Gabriela <gabi844@hotmail.com>
2025-09-25 19:50:45 -0500

    Agregando operadores logicos en el evaluador de edad

--- src/EvaluadorEdad.java
+++ src/EvaluadorEdad.java
@@ -5,10 +5,10 @@ public class EvaluadorEdad {
 Scanner scanner = new Scanner(System.in);
  System.out.println("Por favor ingrese su edad");
  int edad = scanner.nextInt();
- if (edad >=18) {
-    System.out.println("Eres mayor de edad");
- } else {
-    System.out.println("Eres menor de edad");
+ if (edad >=18 && edad < 65) {
+    System.out.println("Usted esta en edad laboral");
+ } if (edad < 18 || edad >=65) {
+    System.out.println("Usted no está en edad laboral.");
  }
  scanner.close();}
 
